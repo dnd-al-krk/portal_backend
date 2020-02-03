250b2551db8a15ad26285f57683422f808c02e5a
Janusz Kamienski <janusz.kamienski@gmail.com>
2020-02-03 17:46:01 +0100

    Revert "bring back decode as it is REQUIRED and returns actually BYTESTRING"
    This reverts commit 5d45c68a55bdaaec7299dd014e8ccd794e22bd24.

--- profiles/models.py
+++ profiles/models.py
@@ -38,7 +38,7 @@ class Profile(models.Model):
             {
                 "user": self.user,
                 "profile": self,
-                "uid": urlsafe_base64_encode(force_bytes(self.user.pk)).decode('utf-8'),
+                "uid": urlsafe_base64_encode(force_bytes(self.user.pk)),
                 "token": account_activation_token.make_token(self.user),
             },
             to=[self.user.email],
