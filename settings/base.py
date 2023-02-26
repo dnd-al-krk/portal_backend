3ce4e7885e33e31a1b3b3d0d2ef6c3cd170daead
Janusz Kamieński <janusz.kamienski@gmail.com>
2023-02-26 18:22:47 +0100

    JWT token refresh time is longer (14 days)

--- settings/base.py
+++ settings/base.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
 
 JWT_AUTH = {
     "JWT_EXPIRATION_DELTA": datetime.timedelta(hours=48),
-    "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=7),
+    "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=14),
     "JWT_ALLOW_REFRESH": True,
 }
 
